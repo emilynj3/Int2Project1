1e0885f3bc61bed6bd095eaf12dac791ddeab390
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-01-31 15:23:35 -0500

    uploaded

---
+++ scripts copy/plugins.js
@@ -0,0 +1,6 @@
+// If you have other JS plugins insert them here
+$("#myHref").on('click', function() {
+    document.getElementById(".myDiv").style.flexGrow = "5";
+     window.location = "http://www.google.com";
+
+});`
